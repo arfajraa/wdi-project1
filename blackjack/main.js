aeb1485d4cf3a781cbe19fb1b5d9348971d79c4f
arfajraa <arfajraa@gmail.com>
2018-12-01 22:22:04 +0300

    fixed some functionality issues and added style to blackjack

--- blackjack/main.js
+++ blackjack/main.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
 
     var suits = ["hearts", "diamonds", "spades", "clubs"];
     var ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
-    // var ranks = ["A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A"];
+    var ranks = ["A", "A", "A", "A", "A", "A", "A", "A", "A", "K", "K", "K", "K"];
     var finalDeck = [];
     var playerHand = [];
     var dealerHand = [];
@@ -21,8 +21,9 @@ $(document).ready(function () {
     var $status = $("#status");
     var $funds = $("#funds");
     var $bet = $("#bet");
-    var $shuffle = $("#shuffle");
+    var $shuffle = $("#reshuffle");
     var $bettingStatus = $("#bettingStatus");
+    var $musicButton = $("#music-control");
 
     //function that generates a new deck using the predefined arrays suits and ranks.
     //each element of the array is a card object that has a suit, a rank, a value in blackjack-
@@ -64,12 +65,13 @@ $(document).ready(function () {
         return newDeck;
     }
 
+    //uses generateDeck() to generate 8 new decks of cards and pushes them to the finalDeck which will be played with
     function createFinalDeck() {
         //clear the deck
         finalDeck.splice(0, finalDeck.length);
         //generate 8 new decks and add their elements to the finalDeck which will be played with
         for (var i = 0; i < 8; i++) {
-            console.log('pushing new deck');
+            // console.log('pushing new deck');
             finalDeck.push.apply(finalDeck, generateDeck());
         }
     }
@@ -84,8 +86,9 @@ $(document).ready(function () {
         }
     }
 
-    
 
+    //initializes a game, this will be called when the user clicks on the deal button.
+    //it will deal 2 cards to the player and 2 cards with one facing down to the dealer 
     function deal() {
         if (parseInt($bet.val()) > funds || parseInt($bet.val()) < 1) {
             $status.html("Invalid bet.")
@@ -110,13 +113,16 @@ $(document).ready(function () {
             $anonCard.hide().appendTo($dealersCards).fadeIn("slow");
             hit("player");
 
-
             $dealersValue.show(true);
             $playersValue.show(true);
         }
     }
 
+    //directly called when the player clicks the hit button
+    //this function will add a card to either the player or the dealer's hand depending on
+    //the passed value which also depends on what step the game is currently on
     function hit(turn) {
+        document.getElementById("dealCard").play();
         var cardFromTop = finalDeck.shift();
         var $newCard = $("<img/>");
         $newCard.addClass("card");
@@ -135,7 +141,16 @@ $(document).ready(function () {
             }
             $newCard.hide().appendTo($playersCards).fadeIn("slow");
             calculateHand("player");
+            console.log(playerHand);
             console.log("player's hand value: " + playerValue);
+
+            if (playerValue == 21) {
+                stand();
+            }
+            else if (playerValue > 21) {
+                status = "playerbust";
+                endGame();
+            }
         }
 
         else if (turn == "dealer") {
@@ -154,16 +169,11 @@ $(document).ready(function () {
             console.log("dealer's hand value: " + dealerValue);
         }
 
-        if (playerValue == 21) {
-            stand();
-        }
-        else if (playerValue > 21) {
-            status = "playerbust";
-            endGame();
-        }
+        $hitButton.prop("disabled", false);
         // console.log("player's hand value: " + playerValue)
     }
 
+    //function that'll calculate the value of the player or dealer's hand depending on the passed value
     function calculateHand(turn) {
         if (turn == "player") {
             playerValue = 0;
@@ -181,15 +191,22 @@ $(document).ready(function () {
         }
     }
 
+    //called when the user gets a blackjack hand (21) or when they click the stand 
+    //button to indicate that they'll stick with the cards in their hand.
+    //it will cause the dealer to reveal their other card and keep hitting until their hand's value
+    //is greater than 17 as per blackjack rules.
     function stand() {
         $dealButton.show(true);
         $hitButton.hide(true);
         $standButton.hide(true);
         $("#dummy").hide(true);
+        // var check = true;
         while (dealerValue < 17) {
+            // check = false;
+
             // window.setInterval(function(){
             hit("dealer");
-
+            // check = true
             // }, 200)
 
         }
@@ -212,6 +229,7 @@ $(document).ready(function () {
     }
 
 
+    //called when the game ends to change the status to show the user the outcome of the game
     function endGame() {
         $status.show(true);
 
@@ -222,7 +240,14 @@ $(document).ready(function () {
         }
         else if (status == "blackjack") {
             $status.html("Blackjack! You win!");
-            funds += (parseFloat($bet.val()) + (parseFloat($bet.val()) * 0.5));
+            console.log("funds: " + funds);
+            funds = funds + parseFloat($bet.val()) + (parseFloat($bet.val()) / 2);
+            console.log("funds: " + funds);
+            $funds.html(funds);
+        }
+        else if (status == "playerwin" && dealerValue > 21) {
+            $status.html("Dealer bust! You win!");
+            funds = funds + parseFloat($bet.val());
             $funds.html(funds);
         }
         else if (status == "playerwin") {
@@ -238,6 +263,7 @@ $(document).ready(function () {
         else if (status == "draw") {
             $status.html("The game is a draw.");
         }
+        console.log("funds: " + funds);
         playerHand = [];
         dealerHand = [];
         $dealButton.show(true);
@@ -249,27 +275,52 @@ $(document).ready(function () {
         // $bet.val(0);
     }
 
+    //utility function just to get a random number within a range, just used to alternate the audio on hit
+    function getRandomInt(max) {
+        return Math.floor(Math.random() * Math.floor(max)) + 1;
+    }
+
+    //by default the finalDeck will be created and shuffled on load, these will also be executed
+    //when the player chooses to reshuffle the deck
     createFinalDeck();
     shuffleDeck(finalDeck);
+    //show the available funds the player has, always starts with $1000
     $funds.html(funds);
     // console.log(finalDeck);
 
-    window.setInterval(function(){
+    //just to emphasize when it's time to place a bet
+    window.setInterval(function () {
         $bettingStatus.toggleClass("betStatusColor");
     }, 250);
 
+    //bunch of event listeners for the buttons
+    //========================================
     $dealButton.on("click", deal);
 
     $hitButton.on("click", function () {
-        hit("player");
+        var hitAudio = document.getElementById(`hit${getRandomInt(4)}`);
+        hitAudio.play();
+        $hitButton.prop("disabled", true);;
+        window.setTimeout(function () {
+            hit("player");
+        }, 1000)
     });
     $shuffle.on("click", function () {
+        var shuffleAudio = document.getElementById(`shuffle`);
+        shuffleAudio.play();
         createFinalDeck();
         shuffleDeck(finalDeck);
-        $status.html("Reshuffled deck.")
+        $status.html("Reshuffled deck.");
     });
 
     $standButton.on("click", stand);
+
+    $musicButton.on("click", function () {
+        var musicBackground = document.getElementById("backgroundMusic");
+        musicBackground.volume = 0.05;
+        musicBackground.play();
+        $musicButton.hide(true);
+    })
     // console.log(playerHand);
     // console.log(playerValue);
 
